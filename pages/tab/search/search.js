63305ad8b11dcb1aa4935a320aa87b5e258cee31
LiaoXiang <lxlove1101@163.com>
2019-02-26 02:07:04 +0800

    新增学院picker

--- pages/tab/search/search.js
+++ pages/tab/search/search.js
@@ -22,6 +22,7 @@ Page({
   onLoad: function(options) {
     var _this = this;
     _this.getActivityTypeRequest();
+    _this.getCollegeRequest();
   },
 
   // 切换tab事件
@@ -81,18 +82,43 @@ Page({
       }
     });
   },
+  //发起请求获取学院基本信息
+  getCollegeRequest: function(){
+    var _this = this;
+    wx.request({
+      url: getApp().data.url + 'QUERY_COLLEGE_BY_ALL',
+      method: 'GET',
+      success: res => {
+        console.log(res);
+        if (res.data.code == 'SUCCESS') {
+          _this.setData({
+            activityCollegeRange: res.data.data.content
+          });
+        } else {
+          wx.showToast({
+            title: '获取信息失败',
+            duration: 1000,
+            mask: true,
+            icon: 'none'
+          });
+        }
+      },
+      fail: err => {
+        console.log(err);
+        wx.showToast({
+          title: '获取信息失败',
+          duration: 1000,
+          mask: true,
+          icon: 'none'
+        });
+      },
+      complete: data => {
+        console.log(getApp().data.url + 'QUERY_COLLEGE_BY_ALL');
+        console.log(data);
+      }
+    });
+  },
 
-  // activityTypeList: [], //获取到的服务器活动类型信息
-
-  // activityType1Value: false, //活动类型1picker-value
-  // activityType1Range: [], //活动类型1picker-range
-  // activityType2Value: false, //活动类型2picker-value
-  // activityType2Range: ['请先选择活动类1'], //活动类型2picker-range
-  // activityType3Value: false, //活动类型3picker-value
-  // activityType3Range: ['请先选择活动类2'], //活动类型3picker-range
-
-  // activityCollegeValue: false, //活动所属学院picker-value
-  // activityCollegeRange: [], //活动所属学院picker-value
   listenerActivityType1: function(e) {
     var index = e.detail.value;
     var activityType2Range = this.data.activityType1Range[index].type2List;
@@ -126,10 +152,10 @@ Page({
     });
   },
   listenerActivityCollege: function(e) {
-    // this.setData({
-    //   serviceAreaValue: e.detail.value,
-    //   'formData.AddressId': this.data.serviceAreaList[e.detail.value].Id
-    // });
+    var index = e.detail.value;
+    this.setData({
+      activityCollegeValue: index,
+    });
   },
   listenerTitle: function(e) {
 
