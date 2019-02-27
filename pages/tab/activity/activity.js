e36cbc7c8e44a2e09e321ec3b654a9b74cf51528
LiaoXiang <lxlove1101@163.com>
2019-02-27 16:46:02 +0800

    删除一些无用文件

--- pages/tab/activity/activity.js
+++ pages/tab/activity/activity.js
@@ -1,54 +1,64 @@
 // pages/activity/activity.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    categories:[
+      {
+        "id": 1,
+        "tag": "declaration",
+        "name": "申报活动",
+        "img": "/images/activity/icon1.png",
+        "color": "#35477d"
+      },
+      {
+        "id": 2,
+        "tag": "record",
+        "name": "申报记录查询",
+        "img": "/images/activity/icon2.png",
+        "color": "#6c5b7b"
+      },
+      {
+        "id": 3,
+        "tag": "collegeActivity",
+        "name": "各学院活动汇总",
+        "img": "/images/activity/icon3.png",
+        "color": "#c06c84"
+      },
+      {
+        "id": 4,
+        "tag": "statistics",
+        "name": "活动情况统计",
+        "img": "/images/activity/icon4.png",
+        "color": "#f67280"
+      },
+      {
+        "id": 5,
+        "tag": "rankOwnClass",
+        "name": "本班排行榜",
+        "img": "/images/activity/icon5.png",
+        "color": "#acc6aa"
+      },
+      {
+        "id": 6,
+        "tag": "other",
+        "name": "待补充",
+        "img": "/images/activity/icon5.png",
+        "color": "#71a0a5"
+      },
+      {
+        "id": 7,
+        "tag": "other",
+        "name": "待补充",
+        "img": "/images/activity/icon5.png",
+        "color": "#77628c"
+      },
+      {
+        "id": 8,
+        "tag": "other",
+        "name": "待补充",
+        "img": "/images/activity/icon5.png",
+        "color": "#905858"
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
   },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
 })
